3e7257e71acfa6df729ae3318c32a888dca3fbec
codrin-horceag <codrinhorceag@gmail.com>
2022-01-26 21:24:18 +0200

    added direction sequence and updated user interface for directions

--- src/com/collections/Main.java
+++ src/com/collections/Main.java
@@ -1,6 +1,7 @@
 package com.collections;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -18,17 +19,49 @@ public class Main {
         locations.put(4, new Location(4, "You are at a pallet."));
         locations.put(5, new Location(5, "You are at a generator."));
 
-        locations.get(1).addExit("W", 2);
+        locations.get(1).addExit("W", 5);
+        locations.get(1).addExit("N", 2);
+        locations.get(1).addExit("S", 4);
+        locations.get(1).addExit("E", 3);
 
+        locations.get(2).addExit("W", 5);
+        locations.get(2).addExit("E", 3);
+
+        locations.get(3).addExit("S", 4);
+        locations.get(3).addExit("Q", 0);
+
+        locations.get(4).addExit("W", 5);
+        locations.get(4).addExit("N", 1);
+        locations.get(4).addExit("E", 3);
+
+        locations.get(5).addExit("E", 1);
+        locations.get(5).addExit("N", 2);
+        locations.get(5).addExit("S", 4);
+
+        System.out.println("Q - Quit game \n" +
+                "N - North \n" +
+                "E - East \n" +
+                "S - South \n" +
+                "W - West \n");
         int loc = 1;
         while(true){
             System.out.println(locations.get(loc).getDescription());
             if(loc == 0){
                 break;
             }
-            System.out.println("Go where?");
-            loc = scanner.nextInt();
-            if(!locations.containsKey(loc)){
+
+            Map<String, Integer> exits = locations.get(loc).getExits();
+            System.out.print("Available directions are: ");
+
+            for (String d : exits.keySet()) {
+                System.out.print(d + ", ");
+            }
+
+            System.out.println();
+            String direction = scanner.nextLine().toUpperCase();
+            if(exits.containsKey(direction)){
+                loc = exits.get(direction);
+            } else {
                 System.out.println("You cannot go in that direction");
             }
         }
